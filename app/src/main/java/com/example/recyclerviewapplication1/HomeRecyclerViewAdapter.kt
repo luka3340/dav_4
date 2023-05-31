5ce779797f17e9eb1376f7cfbc5919832bf55d27
student <student@BTU.local>
2023-05-31 14:26:04 +0400

    adapter added

--- app/src/main/java/com/example/recyclerviewapplication1/HomeRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/recyclerviewapplication1/HomeRecyclerViewAdapter.kt
@@ -1,21 +1,30 @@
 package com.example.recyclerviewapplication1
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.recyclerviewapplication1.databinding.ItemViewBinding
 
 class HomeRecyclerViewAdapter(val dataList : MutableList<Email>) : RecyclerView.Adapter<HomeRecyclerViewAdapter.HomeRecyclerViewViewHolder>() {
 
     inner class HomeRecyclerViewViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-
+        val binding = ItemViewBinding.bind(itemView)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeRecyclerViewViewHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_view, parent, false)
+        return HomeRecyclerViewViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: HomeRecyclerViewViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val item = dataList[position]
+        holder.binding.apply {
+            emailAuthorTV.text = item.author
+            emailSubjectTV.text = item.subject
+            emailContentTV.text = item.content
+//            imageView.setImageResource(item.image!!)
+        }
     }
 
     override fun getItemCount() = dataList.size
